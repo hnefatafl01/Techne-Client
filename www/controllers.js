e5c8859a6e56946a72a0dfb98e8645c7d9377883
Kevin Erickson <hnefatafl@Kevins-MacBook-Air.local>
2017-03-13 11:05:08 -0600

    fixed nvd3 dependencies

--- www/controllers.js
+++ www/controllers.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module('app.controllers', [])
+  angular.module('app.controllers', ['nvd3'])
 
   .controller('GoalCtrl', function($state, GoalService){
     const vm = this;
@@ -26,67 +26,66 @@
 
     vm.styleStatus = function() {
       console.log('removeClass');
-
-      //fix hover 
-      angular.element( document.querySelector( '#goalStatus' ) ).removeClass("ion-ios-pulse");
-      var myEl = angular.element( document.querySelector( '#goalStatus' ) )
-
-      myEl.addClass("ion-ios-pulse-strong");
+      //fix hover
+      // angular.element( document.querySelector( '#goalStatus' ) ).removeClass("ion-ios-pulse");
+      // var myEl = angular.element( document.querySelector( '#goalStatus' ) )
+      //
+      // myEl.addClass("ion-ios-pulse-strong");
     }
 
-    // vm.options = {};
-    // vm.data = {};
-    // vm.options = {
-    //   chart: {
-    //     type: 'pieChart',
-    //     height: 200,
-    //     x: function(d){return d.key;},
-    //     y: function(d){return d.y;},
-    //     showLabels: true,
-    //     duration: 500,
-    //     labelThreshold: 0.01,
-    //     labelSunbeamLayout: true,
-    //     legend: {
-    //       margin: {
-    //         top: 5,
-    //         right: 35,
-    //         bottom: 5,
-    //         left: 0
-    //       }
-    //     }
-    //   }
-    // };
-    //
-    // vm.data = [
-    //   {
-    //     key: "One",
-    //     y: 5
-    //   },
-    //   {
-    //     key: "Two",
-    //     y: 2
-    //   },
-    //   {
-    //     key: "Three",
-    //     y: 9
-    //   },
-    //   {
-    //     key: "Four",
-    //     y: 7
-    //   },
-    //   {
-    //     key: "Five",
-    //     y: 4
-    //   },
-    //   {
-    //     key: "Six",
-    //     y: 3
-    //   },
-    //   {
-    //     key: "Seven",
-    //     y: .5
-    //   }
-    // ];
+    vm.options = {};
+    vm.data = {};
+    vm.options = {
+      chart: {
+        type: 'pieChart',
+        height: 200,
+        x: function(d){return d.key;},
+        y: function(d){return d.y;},
+        showLabels: true,
+        duration: 500,
+        labelThreshold: 0.01,
+        labelSunbeamLayout: true,
+        legend: {
+          margin: {
+            top: 5,
+            right: 35,
+            bottom: 5,
+            left: 0
+          }
+        }
+      }
+    };
+
+    vm.data = [
+      {
+        key: "One",
+        y: 5
+      },
+      {
+        key: "Two",
+        y: 2
+      },
+      {
+        key: "Three",
+        y: 9
+      },
+      {
+        key: "Four",
+        y: 7
+      },
+      {
+        key: "Five",
+        y: 4
+      },
+      {
+        key: "Six",
+        y: 3
+      },
+      {
+        key: "Seven",
+        y: .5
+      }
+    ];
   })
 
   .controller('GoalDetailCtrl', function($state, GoalService){
